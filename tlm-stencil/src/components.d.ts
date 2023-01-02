9aabe67923d4a57f9b1bd9bc4fac9898401a83b5
tabeablnk <tabeablenk@gmail.com>
2023-01-02 14:38:07 +0100

    commented mw-button to have a running site

--- tlm-stencil/src/components.d.ts
+++ tlm-stencil/src/components.d.ts
@@ -5,34 +5,7 @@
  * It contains typing information for all components that exist in this project.
  */
 import { HTMLStencilElement, JSXBase } from "@stencil/core/internal";
-import { Target } from "./components/mw-button/mw-button";
 export namespace Components {
-    interface MwButton {
-        /**
-          * Visually and functionally disable button
-         */
-        "disabled"?: boolean;
-        /**
-          * If provided the button will act as a link
-         */
-        "href"?: string;
-        /**
-          * Label to be displayed
-         */
-        "label"?: string;
-        /**
-          * Use secondary button style
-         */
-        "secondary"?: boolean;
-        /**
-          * If using href the target prop can be provided
-         */
-        "target"?: Target;
-        /**
-          * Must be provided for automated testing
-         */
-        "testId": string;
-    }
     interface MwRadioButton {
         "checked"?: boolean;
         "disabled"?: boolean;
@@ -40,17 +13,7 @@ export namespace Components {
         "value": string;
     }
 }
-export interface MwButtonCustomEvent<T> extends CustomEvent<T> {
-    detail: T;
-    target: HTMLMwButtonElement;
-}
 declare global {
-    interface HTMLMwButtonElement extends Components.MwButton, HTMLStencilElement {
-    }
-    var HTMLMwButtonElement: {
-        prototype: HTMLMwButtonElement;
-        new (): HTMLMwButtonElement;
-    };
     interface HTMLMwRadioButtonElement extends Components.MwRadioButton, HTMLStencilElement {
     }
     var HTMLMwRadioButtonElement: {
@@ -58,38 +21,10 @@ declare global {
         new (): HTMLMwRadioButtonElement;
     };
     interface HTMLElementTagNameMap {
-        "mw-button": HTMLMwButtonElement;
         "mw-radio-button": HTMLMwRadioButtonElement;
     }
 }
 declare namespace LocalJSX {
-    interface MwButton {
-        /**
-          * Visually and functionally disable button
-         */
-        "disabled"?: boolean;
-        /**
-          * If provided the button will act as a link
-         */
-        "href"?: string;
-        /**
-          * Label to be displayed
-         */
-        "label"?: string;
-        "onClickEmitter"?: (event: MwButtonCustomEvent<string>) => void;
-        /**
-          * Use secondary button style
-         */
-        "secondary"?: boolean;
-        /**
-          * If using href the target prop can be provided
-         */
-        "target"?: Target;
-        /**
-          * Must be provided for automated testing
-         */
-        "testId": string;
-    }
     interface MwRadioButton {
         "checked"?: boolean;
         "disabled"?: boolean;
@@ -97,7 +32,6 @@ declare namespace LocalJSX {
         "value"?: string;
     }
     interface IntrinsicElements {
-        "mw-button": MwButton;
         "mw-radio-button": MwRadioButton;
     }
 }
@@ -105,7 +39,6 @@ export { LocalJSX as JSX };
 declare module "@stencil/core" {
     export namespace JSX {
         interface IntrinsicElements {
-            "mw-button": LocalJSX.MwButton & JSXBase.HTMLAttributes<HTMLMwButtonElement>;
             "mw-radio-button": LocalJSX.MwRadioButton & JSXBase.HTMLAttributes<HTMLMwRadioButtonElement>;
         }
     }
--- tlm-stencil/src/components/mw-button/mw-button.tsx
+++ tlm-stencil/src/components/mw-button/mw-button.tsx
@@ -1,246 +1,246 @@
-import { Component, Element, Event, EventEmitter, h, Prop } from '@stencil/core';
-import { css } from '@emotion/css';
-import { getFontWeightValue } from '../../utils/utils';
-import { HTMLStencilElement } from '@stencil/core/internal';
-import {
-  mwComponentButtonPrimaryColorBgDefault,
-  mwComponentButtonBorderWidthPrimaryTextDefault,
-  mwComponentButtonPrimaryColorFgDefault,
-  mwComponentButtonPrimaryColorBgHover,
-  mwComponentButtonPrimaryColorFgHover,
-  mwComponentButtonPrimaryColorBgPressed,
-  mwComponentButtonPrimaryColorFgFocused,
-  mwComponentButtonPrimaryColorBgDisabled,
-  mwComponentButtonPrimaryColorFgDisabled,
-  mwComponentButtonSecondaryTextColorFgDisabled,
-  mwComponentButtonSecondaryTextColorFgFocused,
-  mwComponentButtonSecondaryTextColorFgHover,
-  mwComponentButtonSecondaryTextColorBgDefault,
-  mwComponentButtonSecondaryTextColorFgDefault,
-  mwComponentButtonBorderWidthSecondaryDefault,
-  mwComponentButtonSecondaryTextColorBorderHover,
-  mwComponentButtonSecondaryTextColorBorderDisabled,
-  mwComponentButtonSecondaryTextColorBorderDefault,
-  mwComponentButtonTypo,
-  mwComponentButtonSecondaryTextColorFgPressed,
-  mwComponentButtonSecondaryTextColorBorderPressed,
-  mwComponentButtonPrimaryColorFgPressed,
-  mwComponentButtonPrimaryColorBgFocused,
-  mwComponentButtonSecondaryTextColorBorderFocused,
-  mwComponentButtonPaddingLr,
-  mwComponentButtonPaddingTb,
-  mwComponentButtonGapBetween,
-  mwComponentButtonBorderRadiusDefault,
-  mwComponentSizeButtonPrimarySecondaryMinWidth,
-  mwComponentButtonBorderWidthSecondaryFocused,
-  mwComponentButtonPaddingIconAll,
-  letterSpacingButtons,
-} from '../../../../tlm-token-farm/dist/js/MW_component.js';
+// import { Component, Element, Event, EventEmitter, h, Prop } from '@stencil/core';
+// import { css } from '@emotion/css';
+// import { getFontWeightValue } from '../../utils/utils';
+// import { HTMLStencilElement } from '@stencil/core/internal';
+// import {
+//   mwComponentButtonPrimaryColorBgDefault,
+//   mwComponentButtonBorderWidthPrimaryTextDefault,
+//   mwComponentButtonPrimaryColorFgDefault,
+//   mwComponentButtonPrimaryColorBgHover,
+//   mwComponentButtonPrimaryColorFgHover,
+//   mwComponentButtonPrimaryColorBgPressed,
+//   mwComponentButtonPrimaryColorFgFocused,
+//   mwComponentButtonPrimaryColorBgDisabled,
+//   mwComponentButtonPrimaryColorFgDisabled,
+//   mwComponentButtonSecondaryTextColorFgDisabled,
+//   mwComponentButtonSecondaryTextColorFgFocused,
+//   mwComponentButtonSecondaryTextColorFgHover,
+//   mwComponentButtonSecondaryTextColorBgDefault,
+//   mwComponentButtonSecondaryTextColorFgDefault,
+//   mwComponentButtonBorderWidthSecondaryDefault,
+//   mwComponentButtonSecondaryTextColorBorderHover,
+//   mwComponentButtonSecondaryTextColorBorderDisabled,
+//   mwComponentButtonSecondaryTextColorBorderDefault,
+//   mwComponentButtonTypo,
+//   mwComponentButtonSecondaryTextColorFgPressed,
+//   mwComponentButtonSecondaryTextColorBorderPressed,
+//   mwComponentButtonPrimaryColorFgPressed,
+//   mwComponentButtonPrimaryColorBgFocused,
+//   mwComponentButtonSecondaryTextColorBorderFocused,
+//   mwComponentButtonPaddingLr,
+//   mwComponentButtonPaddingTb,
+//   mwComponentButtonGapBetween,
+//   mwComponentButtonBorderRadiusDefault,
+//   mwComponentSizeButtonPrimarySecondaryMinWidth,
+//   mwComponentButtonBorderWidthSecondaryFocused,
+//   mwComponentButtonPaddingIconAll,
+//   letterSpacingButtons,
+// } from '../../../../tlm-token-farm/dist/js/MW_component.js';
 
-// // figma exports percentage value instead of em
-// // workaround to convert it to float
-// const letterSpacing = Number(letterSpacingButtons.replace('%', '').trim()) / 100;
+// // // figma exports percentage value instead of em
+// // // workaround to convert it to float
+// // const letterSpacing = Number(letterSpacingButtons.replace('%', '').trim()) / 100;
 
-const base = css`
-  appearance: none;
-  text-decoration: none;
-  padding: ${mwComponentButtonPaddingTb} ${mwComponentButtonPaddingLr};
-  font-family: '${mwComponentButtonTypo.fontFamily}';
-  letter-spacing: ${letterSpacing}em;
-  line-height: ${mwComponentButtonTypo.lineHeight}px;
-  font-weight: ${getFontWeightValue(mwComponentButtonTypo.fontWeight)};
-  font-size: ${mwComponentButtonTypo.fontSize}px;
-  border-radius: ${mwComponentButtonBorderRadiusDefault}px;
-  min-width: ${mwComponentSizeButtonPrimarySecondaryMinWidth};
-`;
+// const base = css`
+//   appearance: none;
+//   text-decoration: none;
+//   padding: ${mwComponentButtonPaddingTb} ${mwComponentButtonPaddingLr};
+//   font-family: '${mwComponentButtonTypo.fontFamily}';
+//   letter-spacing: ${letterSpacing}em;
+//   line-height: ${mwComponentButtonTypo.lineHeight}px;
+//   font-weight: ${getFontWeightValue(mwComponentButtonTypo.fontWeight)};
+//   font-size: ${mwComponentButtonTypo.fontSize}px;
+//   border-radius: ${mwComponentButtonBorderRadiusDefault}px;
+//   min-width: ${mwComponentSizeButtonPrimarySecondaryMinWidth};
+// `;
 
-// const primaryButtonStyles = css`
-//   ${base};
-//   border: ${mwComponentButtonBorderWidthPrimaryTextDefault};
-//   color: ${mwComponentButtonPrimaryColorFgDefault};
-//   background-color: ${mwComponentButtonPrimaryColorBgDefault};
-//   &:hover {
-//     background-color: ${mwComponentButtonPrimaryColorBgHover};
-//     color: ${mwComponentButtonPrimaryColorFgHover};
-//   }
-//   &:focus {
-//     background-color: ${mwComponentButtonPrimaryColorBgFocused};
-//     color: ${mwComponentButtonPrimaryColorFgFocused};
-//   }
-//   &:active {
-//     background-color: ${mwComponentButtonPrimaryColorBgPressed};
-//     color: ${mwComponentButtonPrimaryColorFgPressed};
-//   }
-//   &:disabled {
-//     background-color: ${mwComponentButtonPrimaryColorBgDisabled};
-//     color: ${mwComponentButtonPrimaryColorFgDisabled};
+// // const primaryButtonStyles = css`
+// //   ${base};
+// //   border: ${mwComponentButtonBorderWidthPrimaryTextDefault};
+// //   color: ${mwComponentButtonPrimaryColorFgDefault};
+// //   background-color: ${mwComponentButtonPrimaryColorBgDefault};
+// //   &:hover {
+// //     background-color: ${mwComponentButtonPrimaryColorBgHover};
+// //     color: ${mwComponentButtonPrimaryColorFgHover};
+// //   }
+// //   &:focus {
+// //     background-color: ${mwComponentButtonPrimaryColorBgFocused};
+// //     color: ${mwComponentButtonPrimaryColorFgFocused};
+// //   }
+// //   &:active {
+// //     background-color: ${mwComponentButtonPrimaryColorBgPressed};
+// //     color: ${mwComponentButtonPrimaryColorFgPressed};
+// //   }
+// //   &:disabled {
+// //     background-color: ${mwComponentButtonPrimaryColorBgDisabled};
+// //     color: ${mwComponentButtonPrimaryColorFgDisabled};
+// //   }
+// // `;
+
+// // const secondaryButtonStyles = css`
+// //   ${base};
+// //   border: 0;
+// //   outline: ${mwComponentButtonBorderWidthSecondaryDefault}px solid;
+// //   outline-offset: ${-2 * mwComponentButtonBorderWidthSecondaryDefault}px;
+// //   color: ${mwComponentButtonSecondaryTextColorFgDefault};
+// //   background-color: ${mwComponentButtonSecondaryTextColorBgDefault};
+// //   outline-color: ${mwComponentButtonSecondaryTextColorBorderDefault};
+// //   &:hover {
+// //     outline-color: ${mwComponentButtonSecondaryTextColorBorderHover};
+// //     color: ${mwComponentButtonSecondaryTextColorFgHover};
+// //   }
+// //   &:focus {
+// //     outline-color: ${mwComponentButtonSecondaryTextColorBorderFocused};
+// //     outline: ${mwComponentButtonBorderWidthSecondaryFocused}px solid;
+// //     outline-offset: ${-2 * mwComponentButtonBorderWidthSecondaryFocused}px;
+// //     color: ${mwComponentButtonSecondaryTextColorFgFocused};
+// //   }
+// //   &:active {
+// //     outline-color: ${mwComponentButtonSecondaryTextColorBorderPressed};
+// //     color: ${mwComponentButtonSecondaryTextColorFgPressed};
+// //   }
+// //   &:disabled {
+// //     outline-color: ${mwComponentButtonSecondaryTextColorBorderDisabled};
+// //     color: ${mwComponentButtonSecondaryTextColorFgDisabled};
+// //   }
+// // `;
+
+// const iconButtonStyles = css`
+//   padding: ${mwComponentButtonPaddingIconAll};
+//   min-width: 0px;
+// `;
+
+// const flexStyles = css`
+//   display: flex;
+//   align-items: center;
+// `;
+
+// const iconStyles = css`
+//   display: inline-block;
+//   height: 18px;
+//   > span {
+//     display: inline-block;
+//     height: 18px;
+//     width: 18px;
 //   }
 // `;
 
-// const secondaryButtonStyles = css`
-//   ${base};
-//   border: 0;
-//   outline: ${mwComponentButtonBorderWidthSecondaryDefault}px solid;
-//   outline-offset: ${-2 * mwComponentButtonBorderWidthSecondaryDefault}px;
-//   color: ${mwComponentButtonSecondaryTextColorFgDefault};
-//   background-color: ${mwComponentButtonSecondaryTextColorBgDefault};
-//   outline-color: ${mwComponentButtonSecondaryTextColorBorderDefault};
-//   &:hover {
-//     outline-color: ${mwComponentButtonSecondaryTextColorBorderHover};
-//     color: ${mwComponentButtonSecondaryTextColorFgHover};
-//   }
-//   &:focus {
-//     outline-color: ${mwComponentButtonSecondaryTextColorBorderFocused};
-//     outline: ${mwComponentButtonBorderWidthSecondaryFocused}px solid;
-//     outline-offset: ${-2 * mwComponentButtonBorderWidthSecondaryFocused}px;
-//     color: ${mwComponentButtonSecondaryTextColorFgFocused};
-//   }
-//   &:active {
-//     outline-color: ${mwComponentButtonSecondaryTextColorBorderPressed};
-//     color: ${mwComponentButtonSecondaryTextColorFgPressed};
-//   }
-//   &:disabled {
-//     outline-color: ${mwComponentButtonSecondaryTextColorBorderDisabled};
-//     color: ${mwComponentButtonSecondaryTextColorFgDisabled};
+// const iconStartStyles = css`
+//   margin-right: ${mwComponentButtonGapBetween};
+//   display: inline-block;
+//   height: 18px;
+//   > span {
+//     display: inline-block;
+//     height: 18px;
+//     width: 18px;
 //   }
 // `;
 
-const iconButtonStyles = css`
-  padding: ${mwComponentButtonPaddingIconAll};
-  min-width: 0px;
-`;
+// const iconEndStyles = css`
+//   margin-left: ${mwComponentButtonGapBetween};
+//   display: inline-block;
+//   height: 18px;
+//   > span {
+//     display: inline-block;
+//     height: 18px;
+//     width: 18px;
+//   }
+// `;
 
-const flexStyles = css`
-  display: flex;
-  align-items: center;
-`;
+// export type Target = '_blank' | '_self' | '_parent' | '_top';
 
-const iconStyles = css`
-  display: inline-block;
-  height: 18px;
-  > span {
-    display: inline-block;
-    height: 18px;
-    width: 18px;
-  }
-`;
+// @Component({
+//   tag: 'mw-button',
+//   shadow: false,
+//   assetsDirs: ['../assets'],
+// })
+// export class MWButton {
+//   @Element() hostElement: HTMLStencilElement;
+//   /**
+//    * Must be provided for automated testing
+//    */
+//   @Prop() testId!: string;
+//   /**
+//    * Visually and functionally disable button
+//    */
+//   @Prop() disabled?: boolean;
+//   /**
+//    * Label to be displayed
+//    */
+//   @Prop() label?: string;
+//   /**
+//    * Use secondary button style
+//    */
+//   @Prop() secondary?: boolean = false;
+//   /**
+//    * If provided the button will act as a link
+//    */
+//   @Prop() href?: string;
+//   /**
+//    * If using href the target prop can be provided
+//    */
+//   @Prop() target?: Target = '_self';
+//   @Event({
+//     bubbles: true,
+//     cancelable: false,
+//     composed: false,
+//   })
+//   clickEmitter: EventEmitter<string>;
 
-const iconStartStyles = css`
-  margin-right: ${mwComponentButtonGapBetween};
-  display: inline-block;
-  height: 18px;
-  > span {
-    display: inline-block;
-    height: 18px;
-    width: 18px;
-  }
-`;
+//   hasIconStartSlot: boolean;
+//   hasIconEndSlot: boolean;
+//   hasIcon: boolean;
+//   hasLabel: boolean;
 
-const iconEndStyles = css`
-  margin-left: ${mwComponentButtonGapBetween};
-  display: inline-block;
-  height: 18px;
-  > span {
-    display: inline-block;
-    height: 18px;
-    width: 18px;
-  }
-`;
+//   componentWillLoad() {
+//     this.hasIconStartSlot = !!this.hostElement.querySelector('[slot="icon-start"]');
+//     this.hasIconEndSlot = !!this.hostElement.querySelector('[slot="icon-end"]');
+//     this.hasIcon = this.hasIconStartSlot || this.hasIconEndSlot;
+//     this.hasLabel = !!this.label;
+//   }
 
-export type Target = '_blank' | '_self' | '_parent' | '_top';
+//   //   handleClick = () => {
+//   //     this.clickEmitter.emit('onClick');
+//   //   };
 
-@Component({
-  tag: 'mw-button',
-  shadow: false,
-  assetsDirs: ['../assets'],
-})
-export class MWButton {
-  @Element() hostElement: HTMLStencilElement;
-  /**
-   * Must be provided for automated testing
-   */
-  @Prop() testId!: string;
-  /**
-   * Visually and functionally disable button
-   */
-  @Prop() disabled?: boolean;
-  /**
-   * Label to be displayed
-   */
-  @Prop() label?: string;
-  /**
-   * Use secondary button style
-   */
-  @Prop() secondary?: boolean = false;
-  /**
-   * If provided the button will act as a link
-   */
-  @Prop() href?: string;
-  /**
-   * If using href the target prop can be provided
-   */
-  @Prop() target?: Target = '_self';
-  @Event({
-    bubbles: true,
-    cancelable: false,
-    composed: false,
-  })
-  clickEmitter: EventEmitter<string>;
-
-  hasIconStartSlot: boolean;
-  hasIconEndSlot: boolean;
-  hasIcon: boolean;
-  hasLabel: boolean;
-
-  componentWillLoad() {
-    this.hasIconStartSlot = !!this.hostElement.querySelector('[slot="icon-start"]');
-    this.hasIconEndSlot = !!this.hostElement.querySelector('[slot="icon-end"]');
-    this.hasIcon = this.hasIconStartSlot || this.hasIconEndSlot;
-    this.hasLabel = !!this.label;
-  }
-
-//   handleClick = () => {
-//     this.clickEmitter.emit('onClick');
-//   };
-
-  render() {
-    if (this.href) {
-      return (
-        <a href={this.href} target={this.target} class={this.secondary ? secondaryButtonStyles : primaryButtonStyles} test-id={this.testId}>
-          {this.hasIconStartSlot && (
-            <span class={this.hasLabel ? iconStartStyles : iconStyles}>
-              <slot name="icon-start"></slot>
-            </span>
-          )}
-          <span>{this.label}</span>
-          {this.hasIconEndSlot && (
-            <span class={this.hasLabel ? iconEndStyles : iconStyles}>
-              <slot name="icon-end"></slot>
-            </span>
-          )}
-        </a>
-      );
-    }
-    return (
-      <button
-        disabled={this.disabled}
-        onClick={this.handleClick}
-        class={`${this.secondary ? secondaryButtonStyles : primaryButtonStyles} ${this.hasIcon && flexStyles} ${!this.hasLabel && iconButtonStyles}`}
-        test-id={this.testId}
-        type="button"
-      >
-        {this.hasIconStartSlot && (
-          <span class={this.hasLabel ? iconStartStyles : iconStyles}>
-            <slot name="icon-start"></slot>
-          </span>
-        )}
-        <span>{this.label}</span>
-        {this.hasIconEndSlot && (
-          <span class={this.hasLabel ? iconEndStyles : iconStyles}>
-            <slot name="icon-end"></slot>
-          </span>
-        )}
-      </button>
-    );
-  }
-}
+//   render() {
+//     if (this.href) {
+//       return (
+//         <a href={this.href} target={this.target} class={this.secondary ? secondaryButtonStyles : primaryButtonStyles} test-id={this.testId}>
+//           {this.hasIconStartSlot && (
+//             <span class={this.hasLabel ? iconStartStyles : iconStyles}>
+//               <slot name="icon-start"></slot>
+//             </span>
+//           )}
+//           <span>{this.label}</span>
+//           {this.hasIconEndSlot && (
+//             <span class={this.hasLabel ? iconEndStyles : iconStyles}>
+//               <slot name="icon-end"></slot>
+//             </span>
+//           )}
+//         </a>
+//       );
+//     }
+//     return (
+//       <button
+//         disabled={this.disabled}
+//         onClick={this.handleClick}
+//         class={`${this.secondary ? secondaryButtonStyles : primaryButtonStyles} ${this.hasIcon && flexStyles} ${!this.hasLabel && iconButtonStyles}`}
+//         test-id={this.testId}
+//         type="button"
+//       >
+//         {this.hasIconStartSlot && (
+//           <span class={this.hasLabel ? iconStartStyles : iconStyles}>
+//             <slot name="icon-start"></slot>
+//           </span>
+//         )}
+//         <span>{this.label}</span>
+//         {this.hasIconEndSlot && (
+//           <span class={this.hasLabel ? iconEndStyles : iconStyles}>
+//             <slot name="icon-end"></slot>
+//           </span>
+//         )}
+//       </button>
+//     );
+//   }
+// }
